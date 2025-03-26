5631d3735a24df21890f6a3dd5b0363c465f4ff0
CalebKendra <calebkendra7@gmail.com>
2025-03-26 13:24:22 -0400

    yes

--- scripts/quiz_form.js
+++ scripts/quiz_form.js
@@ -3,21 +3,24 @@ const data = `"Pizza is good", "yes", "no", "pineapple", "pie", 3
 const quizData = data.split('\n').map(row => row.split(',').map(cell => cell.trim()));
 const amount = 2;
 const form = document.getElementById("quiz-form");
-let formString = '';
+let formString = '<h1>Quiz</h1>';
 
 for (let i = 0; i < amount; i++) {
   formString += `
   <div class="question">
-      <input type="radio" id="html" value="HTML">
-      <label for="html">HTML</label><br>
-      <input type="radio" id="css" value="CSS">
-      <label for="css">CSS</label><br>
-      <input type="radio" id="javascript" value="JavaScript">
-      <label for="javascript">JavaScript</label>
-      <input type="radio" id="${quizData[i][4]}" value="JavaScript">
-      <label for="javascript">${quizData[i][4]}</label>
+      <h3>${quizData[i][0]}</h3>
+      <input type="radio" name="${i}" id="${quizData[i][1]}" value="${quizData[i][1]}">
+      <label for="${quizData[i][1]}">${quizData[i][1]}</label><br>
+      <input type="radio" name="${i}" id="${quizData[i][2]}" value="${quizData[i][2]}">
+      <label for="${quizData[i][2]}">${quizData[i][2]}</label><br>
+      <input type="radio" name="${i}" id="${quizData[i][3]}" value="${quizData[i][3]}">
+      <label for="${quizData[i][3]}">${quizData[i][3]}</label>
+      <input type="radio" name="${i}" id="${quizData[i][4]}" value="${quizData[i][4]}">
+      <label for="${quizData[i][4]}">${quizData[i][4]}</label>
   </div>
   `;
 }
+
+formString += `<button type="submit">Submit</button>`;
 
 form.innerHTML = formString;
